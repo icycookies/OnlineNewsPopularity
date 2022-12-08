34d0f8f7c5bc1da45615c134bd7b5b8700cf8432
yyhslyz <164326235@qq.com>
2022-12-08 16:33:51 +0800

    Data preprocessing and splitting

---
+++ data.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+
+def load_data(path):
+    df = pd.read_csv(path)
+    df = df[df[' n_unique_tokens'] <= 1]
+    data = df.to_numpy()[:, 2:].astype(float)
+    log_rows = [1, 5, 6, 7, 8, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+    for i in range(data.shape[1]):
+        if i in log_rows:
+            data[:, i] = np.log(data[:, i] - np.min(data[:, i]) + 1)
+        data[:, i] = MinMaxScaler().fit_transform(data[:, i].reshape(-1, 1)).reshape(1, -1)[0]
+    return data[:, :-1], data[:, -1]
+
+def split(x, y, train_ratio, val_ratio, test_ratio):
+    assert train_ratio + val_ratio + test_ratio == 1
+    num_train_val = int(len(y) * (train_ratio + val_ratio))
+    test_x, test_y = x[num_train_val:, :], y[num_train_val:]
+    perm = np.random.permutation(num_train_val)
+    num_train = int(len(y) * train_ratio)
+    train_x, train_y = x[perm[:num_train], :], y[perm[:num_train]]
+    val_x, val_y = x[perm[num_train:], :], y[perm[num_train:]]
+    return train_x, train_y, val_x, val_y, test_x, test_y
+
+if __name__ == "__main__":
+    x, y = load_data('./OnlineNewsPopularity.csv')
+    print(x.shape, y.shape)
+    train_x, train_y, val_x, val_y, test_x, test_y = split(x, y, 0.8, 0.1, 0.1)
+    print(train_x.shape, train_y.shape, val_x.shape,val_y.shape, test_x.shape, test_y.shape)
